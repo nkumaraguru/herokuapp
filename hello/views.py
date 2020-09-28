e203faef240664dfc36406a07133b5eab271d38c
nkumaraguru <kumaraguru@gmail.com>
2020-09-28 17:39:21 +0530

    test tweet

--- hello/views.py
+++ hello/views.py
@@ -3,6 +3,13 @@ from django.http import HttpResponse
 import requests
 from .models import Greeting
 import os
+import tweepy
+
+APP_KEY = 'V4mkmenWVWeZimYcK9jzWBS0F'
+APP_SECRET = 'RUyMKeZzonXUWY9PdfytLBBVI4LqgAYiSUGSRLYW0xQMsh3RmP'
+BEARER_TOKEN = 'AAAAAAAAAAAAAAAAAAAAAAd5IAEAAAAABg3CZmaU1d01gnBfLwgBaqazPvc%3D5WcoGYXGLD3uJKMoWDpt3rczZYw9a0eYLtvG1pYM0QOXXm9sVo'
+ACCESS_TOKEN = '39713422-4zTBRtTp1WvwxtvsBdaeb2xsPaVOhW5Fzx12woBts'
+ACCESS_TOKEN_SECRET = 'yNUQeakiGq33istzn4RZAHG7Y6QvQtvC21rfxqGJX53mE'
 
 # Create your views here.
 def index(request):
@@ -11,9 +18,15 @@ def index(request):
     # r = requests.get('http://httpbin.org/status/418')
     # print(r.text)
     # return HttpResponse('<pre>' + r.text + '</pre>')
-    times = int(os.environ.get('TIMES',3))
-    return HttpResponse('Hello! ' * times)
+    # times = int(os.environ.get('TIMES',3))
+    # return HttpResponse('Hello! ' * times)
+    
+    auth = tweepy.OAuthHandler(APP_KEY, APP_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
+    api = tweepy.API(auth)
+    status = api.update_status('Welcome1234 !!!')
+    return HttpResponse(str(status.created_at))
 
 def db(request):
 
